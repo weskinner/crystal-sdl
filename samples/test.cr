6200da6a005022cf7615f52f0147765f9ac9571a
Farley Knight <farleyknight@gmail.com>
2014-06-21 15:48:43 -0400

    test2 is redundant

--- samples/test.cr
+++ samples/test.cr
@@ -8,7 +8,6 @@ SDL.show_cursor
 
 surface = SDL.set_video_mode width, height, 32, LibSDL::DOUBLEBUF | LibSDL::HWSURFACE | LibSDL::ASYNCBLIT
 
-
 loop do
   SDL.poll_events do |event|
     if event.type == LibSDL::QUIT || event.type == LibSDL::KEYDOWN
