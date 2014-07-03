1be62d4f771d1371cb5a99906a8cf6d129650167
Farley Knight <farleyknight@gmail.com>
2014-07-02 21:26:18 -0400

    Add Point#+

--- samples/point.cr
+++ samples/point.cr
@@ -1,7 +1,13 @@
+# TODO: What makes Point different from vector?
+# Perhaps Point is just struct with some methods?
 class Point
   property :x
   property :y
 
   def initialize(@x, @y)
+  end
+
+  def +(other)
+    Point.new(self.x + other.x, self.y + other.y)
   end
 end
