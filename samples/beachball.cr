c816cbb96cb0aabb6f7b8ff868c29623bd2d621b
Farley Knight <farleyknight@gmail.com>
2014-07-02 21:26:51 -0400

    Refactor to using the Ball class

--- samples/beachball.cr
+++ samples/beachball.cr
@@ -1,41 +1,104 @@
 require "../lib/sdl"
 require "../lib/sdl/game"
 require "point"
+require "vector"
 
-width          = 640
-height         = 480
+width  = 640
+height = 480
 
 # TODO: Turn this into two vectors!
 # Does crystal have a good set of primitive math libraries?
 # Can we contribute simple stuff like Point and Vector?
 
-x              = 0
-x_speed        = 10
+class Ball
 
-y              = 0
-y_speed        = 10
+  property :point
+  property :speed
+  property :image
 
-beachball      = SDL::Image.new("#{__DIR__}/beach_ball.png")
-if beachball.nil?
-  raise "Couldn't load beachball! Error: #{SDL.error}"
+  def initialize(@point, @speed, @image)
+  end
+
+  def x
+    @point.x
+  end
+
+  def x=(other)
+    @point.x = other
+  end
+
+  def x_speed
+    @speed.x
+  end
+
+  def x_speed=(other)
+    @speed.x = other
+  end
+
+  def y
+    @point.y
+  end
+
+  def y=(other)
+    @point.y = other
+  end
+
+  def y_speed
+    @speed.y
+  end
+
+  def y_speed=(other)
+    @speed.y = other
+  end
+
+  def width
+    image.width
+  end
+
+  def height
+    image.height
+  end
+
+  def x_collides?(screen)
+    ((@point.x + width) > screen.width) || (x < 0)
+  end
+
+  def y_collides?(screen)
+    ((@point.y + height) > screen.height) || (y < 0)
+  end
+
+  def move!
+    @point.x -= @speed.x
+    @point.y -= @speed.y
+  end
+
+  def draw_onto(screen)
+    @image.draw_onto(screen, @point)
+  end
+
 end
+
+pos    = Point.new(0, 0)
+speed  = Vector.new(10, 10)
+image  = SDL::Image.new("#{__DIR__}/beach_ball.png")
+
+ball   = Ball.new(pos, speed, image)
 
 Game.go(width, height) do |screen|
   Game.exit_on_event!
 
-  if (x + beachball.width > width) || (x < 0)
-    x_speed = (-x_speed)
+  if ball.x_collides?(screen)
+    ball.x_speed = (-ball.x_speed)
   end
 
-  if (y + beachball.height > height) || (y < 0)
-    y_speed = (-y_speed)
+  if ball.y_collides?(screen)
+    ball.y_speed = (-ball.y_speed)
   end
 
-  x += x_speed
-  y += y_speed
+  ball.move!
 
   screen.fill(SDL::Color.from_hex("ffffff"))
-  beachball.draw_onto(screen, Point.new(x, y))
+  ball.draw_onto(screen)
 
   screen.flip
 end
